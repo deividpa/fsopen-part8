3a403e3477ffb763619e0d39a025597a262cfa73
David Perez <deividpa022@gmail.com>
2024-09-30 20:40:45 -0500

    feat: Part 8.4 is now functional

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -119,7 +119,12 @@ const resolvers = {
     Query: {
       bookCount: () => books.length,
       authorCount: () => authors.length,
-      allBooks: () => books,
+      allBooks: (root, args) => {
+        if (args.author) {
+          return books.filter(book => book.author === args.author)
+        }
+        return books
+      },
       allAuthors: () => authors.map(author => ({
         name: author.name,
         bookCount: books.filter(book => book.author === author.name).length
